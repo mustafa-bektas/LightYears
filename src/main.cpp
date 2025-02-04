892f1d3b698cd1e3ddfb9a1d69256657b8c8c05c
mustafa-bektas <151761340+mustafa-bektas@users.noreply.github.com>
2025-02-04 15:49:07 +0300

    updated folder structure

--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <memory>
-#include <SFML/Graphics.hpp>
-#include "../include/framework/Application.h"
-
-int main()
-{
-    auto app = std::make_unique<ly::Application>();
-    app->Run();
-}
